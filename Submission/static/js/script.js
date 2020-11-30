b843c760a1637e6224caa2494d9b4fbb141da1b0
Jonell Balanay <jonellbalanay@developersinstitute.co.nz>
2020-11-30 22:19:32 +1300

    Get chatbot reply with valid answers

--- Submission/static/js/script.js
+++ Submission/static/js/script.js
@@ -2,36 +2,56 @@ const decisionTree = {
   question: "Does it have a characteristics of a Plant?",
   yes: {
     question: "Which part of a Plant? a Leaf or Wood?",
-    leaf: "Black Zetsu",
+    leaf:{
+      answer: "You're dealing with Black Zetsu!"
+    }, 
     wood: {
       question: "looks Processed of Whittled?",
-      processed: "Konan",
-      whittled: "Sasori",
-    }
+      processed:{
+        answer: "You're dealing with Konan!"
+      }, 
+      whittled: {
+        answer: "You're dealing with Sasori!"
+      }, 
+    },
   },
   no: {
     question: "Has a Short or Long Hair? or maybe its Covered?",
-    covered: "Kakuzu",
+    covered: {
+      answer: "You're dealing with Kakuzu!"
+    }, 
     long: {
       question: "Is he Loud?",
-      yes: "Deidara",
-      no: "Itachi"
+      yes: {
+        answer: "You're dealing with Deidara!"
+      }, 
+      no: {
+        answer: "You're dealing with Itachi!"
+      }, 
     },
     short: {
       question: "Does he act like a Child?",
-      yes:{
+      yes: {
         question: "How's the face? looks Normal or Masked?",
-        normal: "Hidan",
-        masked: "Tobi"
+        normal:{
+          answer: "You're dealing with Hidan!"
+        }, 
+        masked:{
+          answer: "You're dealing with Tobi!"
+        }, 
       },
-      no:{
+      no: {
         question: "Blue or Orange?",
-        blue: "Kisame",
-        orange: "Pein"
-      }
-    }
-  }
-}
+        blue:{
+          answer: "You're dealing with Kisame!"
+        }, 
+        orange:{
+          answer: "You're dealing with Pein!"
+        }, 
+      },
+    },
+  },
+};
 
 // array to track the chat messages
 const chatLogs = [];
@@ -40,53 +60,81 @@ const chatLogs = [];
  *
  * FINISH THIS FUNCTION!
  */
+
+const getName = (msg) => {
+  name = msg;
+  if (name === "") {
+    name = undefined;
+    return "Please enter your name.";
+  }
+  if (nameConfirmed) {
+    return "You're name has been updated";
+  } else {
+    nameConfirmed = false;
+    return `So your name is <b>${name}</b>, is this correct?`;
+  }
+};
+
 let currentDecision = decisionTree;
 
 let name;
 let nameConfirmed;
 
-const yesReplies = ["yup", "yes", "okay", "ok"];
-
-const getName = (msg) => {
-  name = msg;
-  if (name === ""){
-    name = undefined;
-    return ("Please enter your name.");
-  }
-  if(nameConfirmed){
-    return "You're name has been updated";
-  }
-  else{
-    nameConfirmed = false;
-   return `So your name is <b>${name}</b>, is this correct?`
-  }
-}
+const yesReplies = ["yup", "yes", "okay", "ok", "yep", "y"];
+const noReplies = [
+  "nah",
+  "no",
+  "n",
+  "i don't think so",
+  "i dont think so",
+  "its not",
+];
+const miscReplies = ["leaf", "wood", "processed", "whittled", ""];
 
 const getBotReply = (msg) => {
   // return "Error unknown...";
-  if (name === undefined){
+  const lcMessage = msg.toLowerCase();
+
+  //validation section
+  if (name === undefined) {
     return getName(msg);
-  }
- else if (nameConfirmed === false){
-    if (yesReplies.includes(msg.toLowerCase())) {
+  } else if (nameConfirmed === false) {
+    if (yesReplies.includes(lcMessage)) {
       nameConfirmed = true;
-      return "Thanks for confirming!";
-    }
-    else{
+      return `Now tell me the characteristics of this villain.<br><b><i>${currentDecision.question}</i></b><br><br>
+      Cool! Nice to meet you, <b>${name}</b>.
+      `;
+    } else {
       name = undefined;
-      return `Well tell me your name`;
+      return `Well tell me your name then.`;
+    }
+  } else if (msg === "") {
+    return "You were speechless, Sorry I don't get it";
+  }
+
+//conditional questions
+  else {
+    switch(true){
+        case yesReplies.includes(lcMessage):
+
+          if(currentDecision){
+            currentDecision = currentDecision.yes;
+            if(currentDecision.question){
+              return (currentDecision.question);
+            }
+             return(currentDecision.answer);
+          }
+
+          break;
+
+        case miscReplies.includes('leaf'):
+          currentDecision = currentDecision.leaf;
+            return(currentDecision.answer);
+  
+        default:
+          return("I don't understand");
     }
   }
- else if (msg === ""){
-    return "You were speechless, Sorry I don't get it"
-  }
-
-
-// else if (nameConfirmed === true){
-//   return "and Now tell me the characteristics of this villain";
-// }
-
-
 };
 
 const renderChatbox = () => {
